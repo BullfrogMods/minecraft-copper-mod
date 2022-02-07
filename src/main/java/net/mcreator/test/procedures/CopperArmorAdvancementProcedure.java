2f5635030d43b2a3fe688ebf33ab193eeaacc6c8
Jeremiah <jeremiah.wambaugh@gmail.com>
2022-02-06 18:33:21 -0600

    Advancement adjusted

--- src/main/java/net/mcreator/test/procedures/CopperArmorAdvancementProcedure.java
+++ src/main/java/net/mcreator/test/procedures/CopperArmorAdvancementProcedure.java
@@ -1,5 +1,10 @@
 package net.mcreator.test.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -12,10 +17,25 @@ import net.minecraft.advancements.Advancement;
 
 import net.mcreator.test.init.CopperModModItems;
 
+import javax.annotation.Nullable;
+
 import java.util.Iterator;
 
+@Mod.EventBusSubscriber
 public class CopperArmorAdvancementProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			Entity entity = event.player;
+			execute(event, entity);
+		}
+	}
+
 	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
 		if (entity == null)
 			return;
 		if (!(entity instanceof ServerPlayer _plr && _plr.level instanceof ServerLevel
